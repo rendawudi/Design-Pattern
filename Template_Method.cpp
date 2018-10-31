a0a03c2e41adae02a0f5f677509231dcf1334d3f
rendawudi <122700138@qq.com>
2018-11-01 00:30:22 +0800

    final template method

--- Template_Method.cpp
+++ Template_Method.cpp
@@ -1,3 +1,23 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
+
+class lib {
+public:
+    void run() //固定的调用步骤
+    {
+        fun1();
+        fun2();
+        fun3();
+        fun4();
+        fun5();
+    }
+
+private:
+    void fun1();
+    void fun2();
+    void fun5();
+
+    virtual void fun3(); //由子类实现
+    virtual void fun4();
+}
